40dee7e0b2cea860d2c2f90be25813541500f948
dhirajanand <dhiraj.na@gmail.com>
2021-01-17 15:33:36 +0530

    Initial commit

---
+++ components/icons/RVLoginSecretIcon.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import Svg, { Path, Rect } from "react-native-svg";
+export const RVLoginSecretIcon = () => {
+    return (<Svg width={24} height={24} viewBox="0 0 24 24" fill="none" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"
+        className="prefix__feather prefix__feather-lock">
+        <Rect x={3} y={11} width={18} height={11} rx={2} ry={2} />
+        <Path d="M7 11V7a5 5 0 0110 0v4" />
+    </Svg>)
+}
